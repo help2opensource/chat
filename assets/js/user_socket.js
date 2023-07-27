2fbb7deff8246f7dea977fa997e1805f9ee7178e
zenzhao <help2opensource@gmail.com>
2023-07-27 00:57:16 -0700

    add send button

--- assets/js/user_socket.js
+++ assets/js/user_socket.js
@@ -59,9 +59,18 @@ socket.connect()
 let channel = socket.channel("room:lobby", {})
 let chatInput         = document.querySelector("#chat-input")
 let messagesContainer = document.querySelector("#messages")
+let sendButton = document.querySelector("#sendButton")
+
 
 chatInput.addEventListener("keypress", event => {
   if(event.key === 'Enter'){
+    channel.push("new_msg", {body: chatInput.value})
+    chatInput.value = ""
+  }
+})
+
+sendButton.addEventListener("click", event => {
+  if(chatInput.value !== ''){
     channel.push("new_msg", {body: chatInput.value})
     chatInput.value = ""
   }
--- test/chat_web/controllers/chat_test.exs
+++ test/chat_web/controllers/chat_test.exs
@@ -1,11 +1,13 @@
 defmodule ChatWeb.UserListTest do
     use ExUnit.Case, async: true
     use Wallaby.Feature
+
+    import Wallaby.Query, only: [css: 2, text_field: 1, button: 1]
   
     @tag timeout: :infinity
     feature "users have names", %{session: session} do
     
-    for item <- 1..10 do
+    for item <- 1..10000 do
       session
       |> visit("/")
       |> find(Query.css(".user", count: 3))
@@ -16,4 +18,12 @@ defmodule ChatWeb.UserListTest do
         
     end
     end
+
+    feature "users can sent message", %{session: session} do
+      session
+      |> visit("/")
+      |> fill_in(text_field("chat-input"), with: "Write my first Wallaby test")
+     
+    end
+
   end
